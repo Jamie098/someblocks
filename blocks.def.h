b2046932090d1334c86cd02bfa29da14fd52fc4a
Jamie <accounts@jamiehartley.com>
2026-03-11 06:17:51 +0000

    Added mem and temp icons

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,15 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"󰍛 ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 30, 0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-	
-	/* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
-	/* {"", "date '+%b %d (%a) %I:%M%p'",					0,		10}, */
+    {" ", "sensors | grep CPU | awk '{print $2}'", 30, 0},
+
+    {"Battery: ", "acpi | awk '{print $4}'", 60, 0},
+
+    {"", "date '+%b %d %I:%M%p'", 60, 0},
 };
 
-
-
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
