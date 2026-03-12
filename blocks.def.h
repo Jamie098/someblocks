a90b19b25d7865027293581c1467e9a4ef359cfb
Jamie <accounts@jamiehartley.com>
2026-03-12 07:15:18 +0000

    Added battery script

--- blocks.def.h
+++ blocks.def.h
@@ -4,12 +4,12 @@ static const Block blocks[] = {
 
     {" ", "sensors | grep CPU | awk '{print $2}'", 30, 0},
 
-    {"Battery: ", "acpi | awk '{print $4}'", 60, 0},
+    {"", "/home/jamie/.local/src/someblocks/scripts/battery", 60, 0},
 
     {"", "date '+%b %d %I:%M%p'", 60, 0},
 };
 
 // sets delimiter between status commands. NULL character ('\0') means no
 // delimiter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = "    ";
+static unsigned int delimLen = 10;
